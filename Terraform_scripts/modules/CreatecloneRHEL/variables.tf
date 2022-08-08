f426aa776f5cfa8ab29d35df6da6d8ac8d699c63
bloggernest <44660748+rashmigupta88@users.noreply.github.com>
2022-08-08 18:58:23 -0400

    Update variables.tf

--- Terraform_scripts/modules/CreatecloneRHEL/variables.tf
+++ Terraform_scripts/modules/CreatecloneRHEL/variables.tf
@@ -4,6 +4,6 @@ variable "datastore" { default = "NFS_CNACES84_UNIX_VMSTORE_01" }
 variable "network" { default = "1115_hosting_proj_B6" }
 variable "VM_name" { default = "terrafrom-testVM" }
 
-variable "vsphere_user" { default = "svc_osfactory@ocblab.eng"}
-variable "vsphere_password" { default = "CRtcLOSA?QU;%0pl"}
+variable "vsphere_user" { default = "username"}
+variable "vsphere_password" { default = "###########"}
 variable "vsphere_server" { default = "10.1.200.40" }
